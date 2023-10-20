73d3af7c0f13d4baf9e547154f916a3b9023cda4
Marsh1100 <margie.b.agu17@gmail>
2023-10-20 15:53:35 -0500

    Se agregan las entidades al context

--- Persistence/ApiDbContext.cs
+++ Persistence/ApiDbContext.cs
@@ -18,6 +18,31 @@ public class ApiDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Rol> Roles { get; set; }
     public DbSet<UserRol> UserRoles { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Municipio> Municipios { get; set; }
+    public DbSet<Insumo> Insumos { get; set; }
+    public DbSet<Prenda> Prendas { get; set; }
+    public DbSet<DetalleOrden> DetalleOrdenes { get; set; }
+    public DbSet<InsumoPrenda> InsumoPrendas { get; set; }
+    public DbSet<InsumoProveedor> Proveedor { get; set; }
+    public DbSet<TipoProteccion> TipoProteciones { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+    public DbSet<Color> Colores { get; set; }
+    public DbSet<Orden> Ordenes { get; set; }
+    public DbSet<Inventario> Inventarios { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Estado> Estados { get; set; }
+    public DbSet<InventarioTalla> InventarioTallas { get; set; }
+    public DbSet<Cargo> Cargos { get; set; }
+    public DbSet<TipoEstado> TipoEstados { get; set; }
+    public DbSet<Talla> Tallas { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+    public DbSet<TipoPersona> TipoPersonas { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<DetalleVenta> DetalleVentas { get; set; }
+    public DbSet<FormaPago> FormaPagos { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
